570ea5d47e9f96d104e41f5b4eb60f4716db8027
shanba <744385751@qq.com>
2017-01-08 22:48:14 +0800

    Create lib_common.h

---
+++ inc/lib_common.h
@@ -0,0 +1,14 @@
+#ifndef __LIB_COMMON_H
+#define __LIB_COMMON_H
+
+#define SUCCESS  0
+#define ERROR    -1
+
+typedef unsigned char uint8
+typedef unsigned short uint16
+typedef unsigned int uint32
+typedef unsigned long uint64
+
+
+
+#endif
